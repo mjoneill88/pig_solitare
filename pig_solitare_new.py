4523347d33d6d88b6f1fccb5dc873712fca3f2ea
Matthew O'Neill <mjoneill1988@gmail.com>
2016-09-26 00:30:01 -0400

    added a bunch new pig classes

--- pig_solitare_new.py
+++ pig_solitare_new.py
@@ -1,29 +1,114 @@
 
 import random
 
-class Base_Pig:
+
+class Eight_Rolls_Pig:
     def __init__(self):
         self.score = 0
+        self.roll_count =7
+    def roll_again(self):
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
 
+class Seven_Rolls_Pig:
+    def __init__(self):
+        self.score = 0
+        self.roll_count =6
+    def roll_again(self):
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
+
+class Six_Rolls_Pig:
+    def __init__(self):
+        self.score = 0
+        self.roll_count =5
+    def roll_again(self):
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
+
+class Five_Rolls_Pig:
+    def __init__(self):
+        self.score = 0
+        self.roll_count =4
+    def roll_again(self):
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
+
+class Four_Rolls_Pig:
+    def __init__(self):
+        self.score = 0
+        self.roll_count =3
+    def roll_again(self):
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
+
+class Fifty_Percent_Pig:
+    def __init__(self):
+        self.score = 0
     def roll_again(self):
         random_number = random.randint(1,2)
         if random_number == 1:
             return True
         else:
             return False
+            
 
-class Five_Pig:
+class Seventy_Percent_Pig:
     def __init__(self):
         self.score = 0
-        self.roll_count =4
-
     def roll_again(self):
-
-        if self.roll_count > 0:
-            self.roll_count -= 1
+        random_number = random.randint(1,10)
+        if random_number <= 7:
             return True
         else:
             return False
+
+class Eighty_Percent_Pig:
+     def __init__(self):
+         self.score = 0
+     def roll_again(self):
+         random_number = random.randint(1,10)
+         if random_number <= 8:
+             return True
+         else:
+             return False
+
+
+class Ninety_Percent_Pig:
+     def __init__(self):
+         self.score = 0
+     def roll_again(self):
+         random_number = random.randint(1,10)
+         if random_number <= 9:
+             return True
+         else:
+             return False
+
+class Messing_With_Stuff_Pig:
+     def __init__(self):
+         self.score = 0
+     def roll_again(self):
+         if self.score < 20:
+             return True
+         else:
+             return False
+
+
 
 
 def roll_dice():
@@ -50,12 +135,18 @@ def play_game(pig):
 def simulate_trials(trials):
     trial_results = []
     while trials > 0:
-        pig_player = Five_Pig()
+        pig_player = Eight_Rolls_Pig()
         play_game(pig_player)
         trial_results.append(pig_player.score)
         pig_player.score = 0
         trials -= 1
     return trial_results
 
-trial_results = simulate_trials(10)
-print(trial_results)
+
+def main():
+    trial_results = simulate_trials(10000)
+    #print(trial_results)
+    print('Number of Trials:', len(trial_results))
+    print(sum(trial_results)/len(trial_results))
+
+main()
