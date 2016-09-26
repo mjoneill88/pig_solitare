eeaa1e90c8ebcc06c4e8b1b06c05b3e96c67bfe3
Matthew O'Neill <mjoneill1988@gmail.com>
2016-09-25 22:26:36 -0400

    fixed problem when you roll a 1

--- pig_solitare_new.py
+++ pig_solitare_new.py
@@ -4,7 +4,6 @@
 
 import random
 
-
 class Base_Pig:
     def __init__(self):
         self.score = 0
@@ -13,6 +12,23 @@ class Base_Pig:
         random_number = random.randint(1,2)
         if random_number == 1:
             return True
+        else:
+            return False
+
+class Five_Pig:
+    def __init__(self):
+        self.score = 0
+        self.roll_count =4
+
+    def roll_again(self):
+
+        if self.roll_count > 0:
+            self.roll_count -= 1
+            return True
+        else:
+            return False
+
+
 
 
 
@@ -23,20 +39,21 @@ def roll_dice():
 def play_game(pig):
     turn_over = False
     dice_score = roll_dice()
-    if roll_dice == 1:
+    if dice_score == 1:
         turn_over = True
     else:
         pig.score += dice_score
         decide_to_roll_again = pig.roll_again()
         while not turn_over and decide_to_roll_again:
             dice_score = roll_dice()
-            if roll_dice == 1:
+            if dice_score == 1:
                 turn_over = True
+                pig.score = 0
             else:
                 pig.score += dice_score
                 decide_to_roll_again = pig.roll_again()
 
-pig_player = Base_Pig()
+pig_player = Five_Pig()
 
 play_game(pig_player)
 
